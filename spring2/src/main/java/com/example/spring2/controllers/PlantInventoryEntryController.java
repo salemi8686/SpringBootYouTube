6aa204c51918045cfd3a0aff560c9c363d17708a
Serik Bakhramov <90543531+salemi8686@users.noreply.github.com>
2022-02-09 22:10:04 +0600

    Add files via upload

---
+++ spring2/src/main/java/com/example/spring2/controllers/PlantInventoryEntryController.java
@@ -0,0 +1,36 @@
+package com.example.spring2.controllers;
+
+import com.example.spring2.models.PlantInventoryEntry;
+import com.example.spring2.models.PlantInventoryEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class PlantInventoryEntryController {
+    @Autowired
+    PlantInventoryEntryRepository repo;
+
+    @GetMapping("/plants")
+    public String list(Model model) {
+        model.addAttribute("plants", repo.findAll());
+        return "plants/list";
+    }
+
+    @GetMapping(value = "/plants/form")
+    public String form(Model model) {
+        model.addAttribute("plant", new PlantInventoryEntry());
+        return "plants/create";
+    }
+
+    @PostMapping(value = "/plants")
+    public String create(PlantInventoryEntry plant) {
+        repo.save(plant);
+        return "redirect:/plants";
+    }
+}
+
+
---
+++ spring2/src/main/java/com/example/spring2/models/PlantInventoryEntryRepository.java
@@ -0,0 +1,7 @@
+package com.example.spring2.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlantInventoryEntryRepository extends JpaRepository<PlantInventoryEntry, Long> {}
